15c1bc1d345047d553c650827a38afa223d855fd
nwxxb <nawwabzia@gmail.com>
2025-09-07 23:03:41 +0700

    django/init: add permission required decorator to check permission

--- django/jwt_authentication/decorators.py
+++ django/jwt_authentication/decorators.py
@@ -14,3 +14,17 @@ def jwt_required(view_func):
 
         return view_func(request, *args, **kwargs)
     return wrapper
+
+def permission_required(permission_name):
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(request, *args, **kwargs):
+            if not request.user.has_perm(permission_name):
+                return JsonResponse({
+                    "error": "Permission Denied",
+                    "message": f"You do not have the required permission: '{permission_name}'",
+                    "status": "forbidden"
+                }, status=403)
+            return view_func(request, *args, **kwargs)
+        return wrapper
+    return decorator
